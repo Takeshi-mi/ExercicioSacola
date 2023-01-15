8dc586b3977bf3c121f81a3ec0a4b25031daa230
Takeshi Miura <101356765+Takeshi-mi@users.noreply.github.com>
2023-01-14 23:14:53 -0300

    Sacola pronta

--- src/Sacola/Fila.java
+++ src/Sacola/Fila.java
@@ -9,5 +9,36 @@ package Sacola;
  * @author Takeshi
  */
 public class Fila {
-    
+    String[] queue;
+    int end;
+
+    Fila(int size) {
+        queue = new String[size];
+        end = -1; // Zero é a primeira posição, -1 é vazio
+    }
+    boolean isEmpty() {
+        return end ==-1;
+    }
+    boolean isFull() {
+        return end ==queue.length-1;
+    }
+
+    void add(String element) {
+        if (!isFull()) {
+            end += 1;
+            queue[end] = element;
+        }
+    }
+    String remove() {
+        if (!isEmpty()) {
+            String removed = queue[0];
+            for (int i=0; i<queue.length-1; i++) {
+                queue[i] = queue[i+1];
+            }
+            queue[end] = null;
+            end -= 1;
+            return removed;
+        }
+        return null;
+    }
 }
--- src/Sacola/Sacola.java
+++ src/Sacola/Sacola.java
@@ -2,20 +2,23 @@
 package Sacola;
 
 import java.util.Arrays;
+import Sacola.Fila;
+import Sacola.Pilha;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Stack;
 
+
 public class Sacola {
-
-
     public static void main(String[] args) {
     // Import com os métodos de Pilha, fila e fila de Prioridade. Pensei em implementar eles, todos mas ia dar trabalho demais. Implementei a pillha só e aí descobri que dava pra importar
     Stack<Integer> Pilha = new Stack<>();  
     Queue<Integer> Fila = new LinkedList<>();
-    PriorityQueue<Integer> FilaPrioridade = new PriorityQueue<>();
+    PriorityQueue<Integer> FilaPrioridade = new PriorityQueue<>(Collections.reverseOrder()); // É necessário Usar o reverseorder pois
+    //por padrão, a classe PriorityQueue em Java remove o menor número. 
     int elemento;
     //Cores
     String azul = "\u001B[34m";
@@ -69,40 +72,63 @@ public class Sacola {
             System.out.print("Remova um elemento na sacola:  ");
             elemento = entrada.nextInt();
   
-            if (Pilha.peek() != elemento) //Peek(espiar) olha o elemento mais recente da lista sem removê-lo
+            if (Pilha.empty() || Pilha.peek() != elemento) //Peek(espiar) olha o elemento mais recente da lista sem removê-lo
                 eUmaPilha = false;
             else
                 Pilha.pop();
 
-            if (Fila.peek() != elemento)
+            if (Fila.isEmpty() || Fila.peek() != elemento)
                 PodeSerFila = false;
             else
-                Fila.poll(); 
+                Fila.remove(); 
 
-            if (FilaPrioridade.peek() != elemento)
+            if (FilaPrioridade.isEmpty() || FilaPrioridade.peek() != elemento)
                 eUmaFilaPrioridade = false;
             else
                 FilaPrioridade.poll(); // Remove o maior elemento
             }
-            //Printar a sacola    
-                System.out.println(azul + "CONTEUDO DA SACOLA" + reset);
+            //Printar a sacola Fila
+                System.out.println(azul + "CONTEUDO DA FILA" + reset);
                 for (int k: Fila){
                     System.out.print(" | "+ azul + k + reset +" | ");
                 }
+                //System.out.println("fila peek = "+ Fila.peek()); era um teste de mesa pra saber onde eu estava errando.
+                System.out.println(" ");
+            
+                
+                //Printar a sacola Fila Prioridade 
+                System.out.println(azul + "CONTEUDO DA fila prioridade" + reset);
+                for (int k: FilaPrioridade){
+                    System.out.print(" | "+ azul + k + reset +" | ");
+                }
+               // System.out.println("Fila de prioridade peek = " + FilaPrioridade.peek());
+                System.out.println(" ");
+                
+                  //Printar a sacola pilha    
+                System.out.println(azul + "CONTEUDO DA PILHA" + reset);
+                for (int k: Pilha){
+                    System.out.print(" | "+ azul + k + reset +" | ");
+                }
+                //System.out.println("Pilha peek = " + Pilha.peek());
                 System.out.println(" ");
         }
     } 
-    if (eUmaPilha && !PodeSerFila && !eUmaFilaPrioridade)
+    if (eUmaPilha == true && PodeSerFila == false && eUmaFilaPrioridade == false)
         System.out.println("\nÉ definitivamente uma pilha.");
+    
     else if (!eUmaPilha && PodeSerFila && !eUmaFilaPrioridade)
         System.out.println("\nÉ definitivamente uma fila.");
+    
     else if (!eUmaPilha && !PodeSerFila && eUmaFilaPrioridade)
         System.out.println("\nÉ definitivamente uma fila de priporidade.");
+    
     else if (!eUmaPilha && !PodeSerFila && !eUmaFilaPrioridade)
         System.out.println("\nÉ impossível. Não pode ser uma pilha, uma fila ou uma fila de prioridade.");
+    
     else{
-        System.out.println("Nao dapra ter certeza. "); // Quando caia em outro caso sem ser esses de cima ficava em branco. 
+        System.out.println("Nao da pra ter certeza. "); // Quando caia em outro caso sem ser esses de cima ficava em branco. 
     }
+
     System.out.println("\nFim do programa.");
     
 
